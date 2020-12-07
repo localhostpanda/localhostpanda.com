4ff17185c5b61bb01cbba5ea354905ad1a54dc6e
Piyush <piyush41y08h@gmail.com>
2020-12-07 14:46:23 +0530

    Add gatsby mdx plugin and sharp library

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,26 +1,40 @@
-const path = require("path")
-
 module.exports = {
   plugins: [
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: `gatsby-source-filesystem`,
       options: {
-        name: "pages",
-        path: path.resolve(__dirname, "src", "pages"),
+        name: `pages`,
+        path: `${__dirname}/src/pages`,
       },
     },
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: `gatsby-source-filesystem`,
       options: {
-        name: "posts",
-        path: path.resolve(__dirname, "src", "posts"),
+        name: `posts`,
+        path: `${__dirname}/src/posts`,
       },
     },
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: `gatsby-source-filesystem`,
       options: {
-        name: "images",
-        path: path.resolve(__dirname, "src", "images"),
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        extensions: [`.md`, `.mdx`],
+        gatsbyRemarkPlugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 1200,
+            },
+          },
+        ],
       },
     },
   ],
